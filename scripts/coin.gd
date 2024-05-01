6b4eebb81dfc7099b87d1c16338f5e8e49e83e61
KDLN <colinjohnsonw@gmail.com>
2024-05-01 16:30:06 -0400

    v.03

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
+@onready var game_manager = %"Game Manager"
 
+@onready var animation_player = $AnimationPlayer
 
 
 func _on_body_entered(body):
-
-	print("+1 coin")
-	queue_free()
+	game_manager.add_point()
+	animation_player.play("Pickup animation")
